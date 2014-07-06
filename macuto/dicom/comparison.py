3aab9476da9b973ba5fec79878c0cca5a0ce84cc
Alexandre M. S <alexsavio@gmail.com>
2014-07-06 22:22:19 +0200

    Improves to dicom clustering

--- macuto/dicom/comparison.py
+++ macuto/dicom/comparison.py
@@ -2,7 +2,6 @@ __author__ = 'alexandre'
 
 import os
 import dicom
-from dicom.dataset import FileDataset
 import logging
 
 from ..exceptions import LoggedError, FileNotFound
@@ -18,47 +17,6 @@ class DistanceMeasure(object):
     pass
 
 
-class DicomFile(FileDataset):
-
-    def __init__(self, file_path, preamble=None, file_meta=None,
-                 is_implicit_VR=True, is_little_endian=True):
-        """
-
-        :param file_path: str
-        Full path and filename to the file.
-        Use None if is a BytesIO.
-
-        :param dataset:
-        some form of dictionary, usually a Dataset from read_dataset()
-
-        :param preamble: the 128-byte DICOM preamble
-
-        :param file_meta: dataset
-        The file meta info dataset, as returned by _read_file_meta,
-        or an empty dataset if no file meta information is in the file
-
-        :param is_implicit_VR: bool
-        True if implicit VR transfer syntax used; False if explicit VR.
-        Default is True.
-
-        :param is_little_endian: bool
-         True if little-endian transfer syntax used; False if big-endian.
-         Default is True.
-        """
-        if not os.path.exists(file_path):
-            raise FileNotFound(file_path)
-
-        try:
-            dcm = dicom.read_file(file_path)
-
-            FileDataset.__init__(self, file_path, dcm, preamble, file_meta,
-                                 is_implicit_VR, is_little_endian)
-
-            self.file_path = os.path.abspath(file_path)
-        except Exception as exc:
-            raise LoggedError(str(exc))
-
-
 class DicomFileDistance(DistanceMeasure):
 
     import Levenshtein
@@ -67,19 +25,24 @@ class DicomFileDistance(DistanceMeasure):
     similarity_measure = Levenshtein.ratio
     inv_sum_weights = 1/sum(field_weights.values())
 
+    def fit(self, file_path1, file_path2):
+        self.dcmf1 = DicomFile(file_path1)
+        self.dcmf2 = DicomFile(file_path2)
+
     def fit_transform(self, file_path1, file_path2):
+        self.fit(file_path1, file_path2)
+        return self.transform()
+
+    def transform(self):
 
         if len(self.field_weights) == 0:
             raise LoggedError('Field weights are not set.')
 
-        dcm1 = DicomFile(file_path1)
-        dcm2 = DicomFile(file_path2)
-
         try:
             dist = 0
             for field_name in self.field_weights:
-                str1 = str(getattr(dcm1, field_name))
-                str2 = str(getattr(dcm2, field_name))
+                str1 = str(getattr(self.dcmf1, field_name))
+                str2 = str(getattr(self.dcmf2, field_name))
 
                 if not str1 or not str2:
                     continue
