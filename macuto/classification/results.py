9d518b70e6df8f15c698f63fa4d36d3da2547fc5
Alexandre M. S <alexsavio@gmail.com>
2014-08-11 13:02:50 +0200

    Correct enlist_cv_results, add enlist_cv_results_from_dict for ClassificationPipeline.

--- macuto/classification/results.py
+++ macuto/classification/results.py
@@ -110,8 +110,75 @@ def classification_metrics(targets, preds, probs=None, labels=None):
     return acc, sens, spec, prec, f1, auc
 
 
-def enlist_cv_results(cv_targets, cv_preds, cv_probs=None):
+def enlist_cv_results_from_dict(cv_targets, cv_preds, cv_probs=None):
+    """Put cv_targets, cv_preds and cv_probs in lists for performance measures.
+    Also returns the set of target labels.
+
+    Parameters
+    ----------
+    cv_targets: dict
+    cv_preds: dict
+    cv_probs: dict
+
+    Returns
+    -------
+    targets, preds, probs, labels
     """
+    targets = []
+    preds = []
+    labels = set()
+
+    if cv_probs is None:
+        probs = None
+    elif len(cv_probs) == 0:
+        probs = None
+    else:
+        probs = []
+
+    #see if it is a LOO result set
+    fold_len = 1
+    for fold in cv_targets:
+        if len(cv_targets[fold]) != fold_len:
+            fold_len = len(cv_targets[fold])
+            break
+
+    #read results
+    for fold in cv_targets:
+        try:
+            if fold_len == 1:
+                trgt = cv_targets[fold][0]
+                pred = cv_preds[fold][0]
+            else:
+                trgt = cv_targets[fold]
+                pred = cv_preds[fold]
+
+            targets.append(trgt)
+            preds.append(pred)
+        except:
+            log.exception('Error accessing classification results.')
+            raise
+
+        if cv_probs is not None:
+            try:
+                if fold_len == 1:
+                    prob = cv_probs[fold][0]
+                else:
+                    prob = cv_probs[fold]
+
+                probs.append(prob)
+            except:
+                log.exception('Error accessing cv_probs.')
+                raise
+
+        for t in np.unique(trgt):
+            labels.add(t)
+
+    return targets, preds, probs, list(labels)
+
+
+def enlist_cv_results(cv_targets, cv_preds, cv_probs=None):
+    """Put cv_targets, cv_preds and cv_probs in lists for performance measures.
+     Also returns the set of target labels.
 
     Parameters
     ----------
@@ -123,42 +190,35 @@ def enlist_cv_results(cv_targets, cv_preds, cv_probs=None):
     -------
     targets, preds, probs, labels
     """
-    targets = []
-    preds   = []
-    probs   = []
-
     if isinstance(cv_targets, dict):
-        c = 0
-        for i in list(cv_targets.keys()):
-            try:
-                targets.append(cv_targets[i])
-                preds.append(cv_preds[i])
-
-                if cv_probs is not None:
-                    if len(cv_probs) > 0:
-                        probs.append(cv_probs[i])
-            except:
-                log.exception("Error joining CV results.")
-                raise
-
-            c += 1
+        return enlist_cv_results_from_dict(cv_targets, cv_preds, cv_probs)
 
     else:
-        for i in np.arange(cv_targets.shape[0]):
-            targets.append(cv_targets[i, :])
-            preds.append(cv_preds[i, :])
+        targets = []
+        preds = []
+        probs = []
+        labels = set()
 
-            if cv_probs is not None:
-                probs.append(cv_probs[i, :, :])
+        try:
+            for fold in np.arange(cv_targets.shape[0]):
+                targets.append(cv_targets[fold, :])
+                preds.append(cv_preds[fold, :])
+
+                for t in np.unique(cv_targets[fold, :]):
+                    labels.add(t)
+
+                if cv_probs is not None:
+                    probs.append(cv_probs[fold, :, :])
+        except:
+            log.exception("Error joining CV results.")
+            raise
 
     if cv_probs is None:
         probs = None
     elif len(cv_probs) == 0:
         probs = None
 
-    labels = np.unique(targets[0])
-
-    return targets, preds, probs, labels
+    return targets, preds, probs, list(labels)
 
 
 def get_cv_classification_metrics(cv_targets, cv_preds, cv_probs=None):
